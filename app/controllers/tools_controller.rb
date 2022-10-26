254e644afc06165e0d59c3a489175e26ac39e3d5
Marina Angeli <marinasimoes@gmail.com>
2022-10-26 14:29:57 -0300

    added new tool action in controller, and new tool basic view

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -7,9 +7,16 @@ class ToolsController < ApplicationController
   end
 
   def new
+    @tool = Tool.new
   end
 
   def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -19,5 +26,11 @@ class ToolsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :price, :delivery)
   end
 end
