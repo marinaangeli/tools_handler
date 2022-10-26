7f61a5bea6a0f65f7e758ea57cfe47d37c709493
Marina Angeli <marinasimoes@gmail.com>
2022-10-26 17:20:50 -0300

    install bundle

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,7 +1,7 @@
 class ToolsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    @tools = Tool.all
+    # @tools = Tool.all
     @tools = policy_scope(Tool)
   end
 
