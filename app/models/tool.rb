0aa1db862150e2ed73f164199e0ed5a75ca79094
Marina Angeli <marinasimoes@gmail.com>
2022-10-28 18:18:30 -0300

    installed Active Records, enables photo for tools, and ssetup tools validation

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,7 +1,8 @@
 class Tool < ApplicationRecord
   belongs_to :user
   has_many :rentals, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, numericality: true
 end
