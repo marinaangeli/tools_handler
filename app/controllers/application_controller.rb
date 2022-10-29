4ab056f578e6620f1b45f44067e771fca79dfd7e
raul chagas <raul_chagas@hotmail.com>
2022-10-29 15:44:04 -0300

    formating done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :state, :city, :area, :address, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :state, :city, :area, :address, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :phone])
   end
 
   private
