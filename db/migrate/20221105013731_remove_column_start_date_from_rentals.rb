f4157f4c82c670a2e075e95a1427ce9271921809
raul chagas <raul_chagas@hotmail.com>
2022-11-05 12:12:13 -0300

    removed migrtaions for dates

--- db/migrate/20221105013731_remove_column_start_date_from_rentals.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnStartDateFromRentals < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :rentals, :start_date, :datetime
-  end
-end
--- db/migrate/20221105013828_remove_column_end_date_from_rentals.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnEndDateFromRentals < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :rentals, :end_date, :datetime
-  end
-end
