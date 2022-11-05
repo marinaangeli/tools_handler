5064a81f31b532a9c82640b13294feb966d2f7f4
Marina Angeli <marinasimoes@gmail.com>
2022-11-05 13:24:58 -0300

    chega

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,7 +82,7 @@ end
 
 @user = @users.sample
 @chave_de_fenda_cruzada = Tool.create(
-  name: "Caixa Sanfonada",
+  name: "Chave de fenda cruzada",
   price: rand(1..200),
   user: @user,
   rating: rand(1..5),
