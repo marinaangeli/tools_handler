bdbfffc52f1b52811a4a60c5bd7f1a418f3f92db
raul chagas <raul_chagas@hotmail.com>
2022-11-05 00:03:36 -0300

    added logic to prevent start_date > end_date

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,6 +4,9 @@ class Rental < ApplicationRecord
 
   before_save :calculate_cost
 
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
   def num_days
     self.days = (end_date.to_i - start_date.to_i) / 86_400
   end
@@ -13,4 +16,13 @@ class Rental < ApplicationRecord
   def calculate_cost
     self.cost = tool.price * (end_date.to_i - start_date.to_i) / 86_400
   end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
+
 end
